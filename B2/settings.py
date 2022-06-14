87478ce5def55cfde1f3713172292f097430ee7b
hsargoli <hsargoli@gmail.com>
2022-06-14 13:10:16 +0430

    first step in create modelORdatabase for our site

--- B2/settings.py
+++ B2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'smsData.apps.SmsdataConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ smsData/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Sms(models.Model):
+    text = models.TextField()
+    
